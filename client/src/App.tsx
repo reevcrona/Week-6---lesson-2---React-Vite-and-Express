3c03e5c19e72be79e18a09a2cfed02ab9670f945
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-02-26 13:56:44 +0100

    addes some conditional styling and a form when you submit it fetches that many colors will att more later

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,11 @@ import axios from "axios";
 import { color } from "./types";
 function App() {
   const [apiData, setApiData] = useState<color[] | null>(null);
-
+  const [inputValue, setInputValue] = useState("");
   const fetchApi = async () => {
     try {
       const response = await axios.get<color[]>(
-        "http://localhost:3000/colors/4"
+        `http://localhost:3000/colors/${inputValue === "" ? "4" : inputValue}`
       );
       console.log(response.data);
       setApiData(response.data);
@@ -16,7 +16,11 @@ function App() {
       console.error(`Unexpected error: ${error}`);
     }
   };
-
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchApi();
+    setInputValue("");
+  };
   useEffect(() => {
     fetchApi();
   }, []);
@@ -26,11 +30,19 @@ function App() {
       return apiData.map((item, index) => {
         return (
           <div
-            className="color-column"
+            className={apiData.length > 5 ? "color-row" : "color-column"}
             style={{ backgroundColor: item.hex, color: item.textColor }}
             key={index}
           >
-            {item.name}
+            <div
+              className={apiData.length > 5 ? "color-info-small" : "color-info"}
+            >
+              <h3>{item.name}</h3>
+              <p>Hexcode: {item.hex}</p>
+              <p>{item.rgb}</p>
+              <p>{item.cmyk}</p>
+              <p>{item.hsl}</p>
+            </div>
           </div>
         );
       });
@@ -39,10 +51,27 @@ function App() {
 
   return (
     <div className="main-container">
-      <form>
-        <input type="text" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="number"
+          placeholder="Amount of colors"
+          value={inputValue}
+          min="1"
+          max="25"
+          required
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Get Colors</button>
       </form>
-      <div className="color-container">{renderData()}</div>
+      <div
+        className={
+          apiData && apiData.length > 5
+            ? "color-container-column"
+            : "color-container"
+        }
+      >
+        {renderData()}
+      </div>
     </div>
   );
 }
